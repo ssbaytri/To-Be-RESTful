d93a4cd61b752f40ac28ba74c0e776c229331567
ssbaytri <azjoker333@gmail.com>
2026-09-03 06:02:35 +0100

    models

---
+++ ex00/Education-Center/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (id, first_name, last_name, role, login, password) VALUES
+    (1, 'Best', 'Teacher', 'TEACHER', 'bteacher', 'teacher123'),
+    (2, 'Second', 'Teacher', 'TEACHER', 'steacher', 'teacher123'),
+    (3, 'Alice', 'Student', 'STUDENT', 'astudent', 'student123'),
+    (4, 'Bob', 'Student', 'STUDENT', 'bstudent', 'student123'),
+    (5, 'Admin', 'Istrator', 'ADMINISTRATOR', 'admin', 'admin123');
