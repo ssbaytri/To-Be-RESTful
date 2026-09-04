da589a6a648720e7e4ffdceae3bfc6928fd1736c
ssbaytri <azjoker333@gmail.com>
2026-09-05 00:30:25 +0100

    IDENTITY PROBLEM

--- ex00/Education-Center/src/main/resources/data.sql
+++ ex00/Education-Center/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO users (id, first_name, last_name, role, login, password) VALUES
-    (1, 'Best', 'Teacher', 'TEACHER', 'bteacher', 'teacher123'),
-    (2, 'Second', 'Teacher', 'TEACHER', 'steacher', 'teacher123'),
-    (3, 'Alice', 'Student', 'STUDENT', 'astudent', 'student123'),
-    (4, 'Bob', 'Student', 'STUDENT', 'bstudent', 'student123'),
-    (5, 'Admin', 'Istrator', 'ADMINISTRATOR', 'admin', 'admin123');
+INSERT INTO users (first_name, last_name, role, login, password)
+VALUES ('Best', 'Teacher', 'TEACHER', 'bteacher', 'teacher123'),
+       ('Second', 'Teacher', 'TEACHER', 'steacher', 'teacher123'),
+       ('Alice', 'Student', 'STUDENT', 'astudent', 'student123'),
+       ('Bob', 'Student', 'STUDENT', 'bstudent', 'student123'),
+       ('Admin', 'Istrator', 'ADMINISTRATOR', 'admin', 'admin123');
