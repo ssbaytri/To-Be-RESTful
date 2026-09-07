ec7050b6c638938e58428e02698df74d885f5b9e
ssbaytri <azjoker333@gmail.com>
2026-09-08 00:47:41 +0100

    MINOR FIX

--- ex01/Education-Center/src/main/resources/data.sql
+++ ex01/Education-Center/src/main/resources/data.sql
@@ -3,4 +3,4 @@ VALUES ('Best', 'Teacher', 'TEACHER', 'bteacher', 'teacher123'),
        ('Second', 'Teacher', 'TEACHER', 'steacher', 'teacher123'),
        ('Alice', 'Student', 'STUDENT', 'astudent', 'student123'),
        ('Bob', 'Student', 'STUDENT', 'bstudent', 'student123'),
-       ('Admin', 'Istrator', 'ADMINISTRATOR', 'admin', 'admin123');
+       ('Admin', 'Istrator', 'ADMINISTRATOR', 'admin', 'admin123') ON CONFLICT (login) DO NOTHING;
