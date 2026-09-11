435ed0d8fc08203cb3314583b1bfc9a1166de2ad
ssbaytri <azjoker333@gmail.com>
2026-09-11 07:58:31 +0100

    Tests

--- ex01/Education-Center/src/test/java/fr/_42/educationcenter/controllers/UsersControllerTest.java
+++ ex01/Education-Center/src/test/java/fr/_42/educationcenter/controllers/UsersControllerTest.java
@@ -1,25 +1,18 @@
 package fr._42.educationcenter.controllers;
 
-import fr._42.educationcenter.dto.UserRequest;
-import fr._42.educationcenter.dto.UserResponse;
 import fr._42.educationcenter.services.UserService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -42,100 +35,37 @@ public class UsersControllerTest {
             }
             """;
 
-    @BeforeEach
-    public void setUp() {
-        when(userService.createUser(any(UserRequest.class)))
-                .thenReturn(new UserResponse(
-                        1L,
-                        "Best",
-                        "Teacher",
-                        "TEACHER",
-                        "bteacher"
-                ));
-
-        when(userService.getUser(1L))
-                .thenReturn(new UserResponse(
-                        1L,
-                        "Best",
-                        "Teacher",
-                        "TEACHER",
-                        "bteacher"
-                ));
-
-        when(userService.updateUser(eq(1L), any(UserRequest.class)))
-                .thenReturn(new UserResponse(
-                        1L,
-                        "New",
-                        "Name",
-                        "ADMINISTRATOR",
-                        "newname"
-                ));
-    }
-
     @Test
-    public void createUserTest() throws Exception {
-        mockMvc.perform(
-                        post("/users")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(USER_JSON)
-                )
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").value(1))
-                .andExpect(jsonPath("$.firstName").value("Best"))
-                .andExpect(jsonPath("$.lastName").value("Teacher"))
-                .andExpect(jsonPath("$.role").value("TEACHER"))
-                .andExpect(jsonPath("$.login").value("bteacher"));
+    public void getAllUsersTest() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isForbidden());
     }
 
     @Test
     public void getUserTest() throws Exception {
         mockMvc.perform(get("/users/1"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(1))
-                .andExpect(jsonPath("$.firstName").value("Best"))
-                .andExpect(jsonPath("$.lastName").value("Teacher"))
-                .andExpect(jsonPath("$.role").value("TEACHER"))
-                .andExpect(jsonPath("$.login").value("bteacher"));
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void createUserTest() throws Exception {
+        mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(USER_JSON))
+                .andExpect(status().isForbidden());
     }
 
     @Test
     public void updateUserTest() throws Exception {
-        mockMvc.perform(
-                        put("/users/1")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(USER_JSON)
-                )
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(1))
-                .andExpect(jsonPath("$.firstName").value("New"))
-                .andExpect(jsonPath("$.lastName").value("Name"))
-                .andExpect(jsonPath("$.role").value("ADMINISTRATOR"))
-                .andExpect(jsonPath("$.login").value("newname"));
-    }
-
-    @Test
-    public void getUpdatedUserTest() throws Exception {
-        when(userService.getUser(1L))
-                .thenReturn(new UserResponse(
-                        1L,
-                        "New",
-                        "Name",
-                        "ADMINISTRATOR",
-                        "newname"
-                ));
-
-        mockMvc.perform(get("/users/1"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(1))
-                .andExpect(jsonPath("$.firstName").value("New"))
-                .andExpect(jsonPath("$.lastName").value("Name"))
-                .andExpect(jsonPath("$.role").value("ADMINISTRATOR"))
-                .andExpect(jsonPath("$.login").value("newname"));
+        mockMvc.perform(put("/users/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(USER_JSON))
+                .andExpect(status().isForbidden());
     }
 
     @Test
     public void deleteUserTest() throws Exception {
         mockMvc.perform(delete("/users/1"))
-                .andExpect(status().isNoContent());
+                .andExpect(status().isForbidden());
     }
 }
